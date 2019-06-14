9ca4e30db030a632d6351a92d475c241f8b2fcb7
Juha Yrjölä <juha.yrjola@iki.fi>
2019-06-14 09:38:32 +0300

    New version of front page

---
+++ utils/colors.py
@@ -0,0 +1,10 @@
+
+
+GHG_MAIN_SECTOR_COLORS = {
+    'Lämmitys': '#FD4F00',
+    'Liikenne': '#FFC61E',
+    'Sähkö': '#BD2719',
+    'Jätteiden käsittely': '#009246',
+    'Teollisuus ja työkoneet': '#F5A3C7',
+    'Maatalous': '#FFE977'
+}
