a30762b544318aaf3cda39fb3f3ea7ca524d17e0
Juha Yrjölä <juha.yrjola@iki.fi>
2019-12-10 11:41:13 +0200

    Switch to using Redis
    pylibmc is not thread safe. 😭

--- common/settings.py
+++ common/settings.py
@@ -11,57 +11,45 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 CACHE_KEY_PREFIX = 'ghgdash-cache'
 CACHE_TYPE = 'simple'
-CACHE_MEMCACHED_SERVERS = ()
+CACHE_REDIS_URL = None
 
 SESSION_TYPE = 'filesystem'
 SESSION_FILE_DIR = os.path.join(BASE_DIR, 'flask_session')
 SESSION_KEY_PREFIX = 'ghgdash-session'
 
-MEMCACHED_URL = os.getenv('MEMCACHED_URL', None)
+REDIS_URL = os.getenv('REDIS_URL', None)
 
 
 def get_cache_config():
-    global CACHE_TYPE, CACHE_MEMCACHED_SERVERS
+    global CACHE_TYPE, CACHE_REDIS_URL
 
-    url = os.getenv('CACHE_BACKEND_URL', None)
-    if not url and MEMCACHED_URL:
-        url = MEMCACHED_URL
+    url = os.getenv('CACHE_REDIS_URL', None)
+    if not url and REDIS_URL:
+        url = REDIS_URL
 
-    if not url:
-        return
-
-    o = urlparse(url)
-    if o.scheme == 'memcached':
-        CACHE_TYPE = 'memcached'
-        CACHE_MEMCACHED_SERVERS = (o.netloc,)
-    elif o.scheme == 'simple':
-        pass
-    else:
-        raise ImproperlyConfigured('Invalid CACHE_BACKEND_URL: %s' % url)
+    if url:
+        CACHE_REDIS_URL = url
+        CACHE_TYPE = 'redis'
 
 
 get_cache_config()
 
 
 def get_session_config():
-    global SESSION_TYPE, SESSION_MEMCACHED
+    global SESSION_TYPE, SESSION_REDIS
 
-    url = os.getenv('SESSION_BACKEND_URL', None)
-    if not url and MEMCACHED_URL:
-        url = MEMCACHED_URL
+    url = os.getenv('SESSION_REDIS_URL', None)
+    if not url and REDIS_URL:
+        url = REDIS_URL
 
-    o = urlparse(url)
-    if o.scheme == 'memcached':
-        import pylibmc
+    if not url:
+        return
 
-        SESSION_TYPE = 'memcached'
+    from redis import Redis
 
-        if not o.netloc and MEMCACHED_URL:
-            o = urlparse(MEMCACHED_URL)
-
-        SESSION_MEMCACHED = pylibmc.Client([o.netloc], binary=True)
-    else:
-        raise ImproperlyConfigured('Invalid SESSION_BACKEND_URL: %s' % url)
+    client = Redis.from_url(url)
+    SESSION_REDIS = client
+    SESSION_TYPE = 'redis'
 
 
 get_session_config()
