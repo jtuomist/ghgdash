3381b9f93b686b60ef0df6344de703fd07d7882b
Juha Yrjölä <juha.yrjola@iki.fi>
2020-01-03 09:58:47 +0200

    Use Redis cache backend also for non-flask invocations

--- common/cache.py
+++ common/cache.py
@@ -2,20 +2,39 @@ import pandas as pd
 from flask_caching import Cache
 
 
-_cache = {}
+_cache_backend = None
+
+
+def _init_local_cache():
+    from common import settings
+    global _cache_backend
+
+    if settings.CACHE_TYPE == 'simple':
+        from flask_caching.backends.simple import SimpleCache
+
+        _cache_backend = SimpleCache()
+    elif settings.CACHE_TYPE == 'redis':
+        from redis import from_url as redis_from_url
+        from flask_caching.backends.rediscache import RedisCache
+
+        _cache_backend = RedisCache(
+            key_prefix=settings.CACHE_KEY_PREFIX,
+            host=redis_from_url(settings.CACHE_REDIS_URL)
+        )
 
 
 def get(key):
-    ret = _cache.get(key)
-    if isinstance(ret, (pd.DataFrame, pd.Series)):
-        ret = ret.copy(deep=True)
-    return ret
+    if _cache_backend is None:
+        _init_local_cache()
+
+    return _cache_backend.get(key)
 
 
 def set(key, val, timeout=None):
-    if isinstance(val, (pd.DataFrame, pd.Series)):
-        val = val.copy(deep=True)
-    _cache[key] = val
+    if _cache_backend is None:
+        _init_local_cache()
+
+    _cache_backend.set(key, val)
 
 
 def init_app(app):
